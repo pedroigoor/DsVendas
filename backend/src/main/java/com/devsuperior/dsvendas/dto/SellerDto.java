604cf8151607b4d9b97b5efb8e97b75bb9d3f3ce
Pedro Almeida <pedro.igor10@live.com>
2021-11-04 19:59:10 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDto.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.enteties.Seller;
+
+public class SellerDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+
+	public SellerDto() {
+
+	}
+
+	public SellerDto(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public SellerDto(Seller seller) {
+		this.id = seller.getId();
+		this.name = seller.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
